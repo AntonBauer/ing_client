6073cc54a47ab812b373c97e1b99429af65c4e7e
Anton Bauer <bauer_av+git@proton.me>
2024-12-28 14:04:13 +0100

    Initial setup

---
+++ tst/IngClient.Tests.Client/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace IngClient.Tests.Client;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
