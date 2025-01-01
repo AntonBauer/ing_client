da3cd5189a3524bc81b7dd2eacd02da6661e6804
Anton Bauer <bauer_av+git@proton.me>
2025-01-01 21:22:29 +0100

    fix property names for token response model

--- src/IngClient.Client/Authentication/Models/TokenResponse.cs
+++ src/IngClient.Client/Authentication/Models/TokenResponse.cs
@@ -1,22 +1,57 @@
+using System.Text.Json.Serialization;
+
 namespace IngClient.Client.Authentication.Models;
 
-internal readonly record struct TokenResponse(
-    string AccessToken,
-    int ExpiresIn,
-    string Scope,
-    string TokenType,
-    string RefreshToken,
-    int RefreshTokenExpiresIn,
-    string ClientId,
-    List<Key> Keys
-);
-internal readonly record struct Key(
-    string Kty,
-    string Alg,
-    string Use,
-    string Kid,
-    string N,
-    string E,
-    string X5t,
-    List<string> X5c
-);
+internal readonly record struct TokenResponse
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; init; }
+
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; init; }
+
+    [JsonPropertyName("scope")]
+    public string Scope { get; init; }
+
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; init; }
+
+    [JsonPropertyName("refresh_token")]
+    public string RefreshToken { get; init; }
+
+    [JsonPropertyName("refresh_token_expires_in")]
+    public int RefreshTokenExpiresIn { get; init; }
+
+    [JsonPropertyName("client_id")]
+    public string ClientId { get; init; }
+
+    [JsonPropertyName("keys")]
+    public List<Key> Keys { get; init; }
+
+    internal readonly record struct Key
+    {
+        [JsonPropertyName("kty")]
+        public string Kty { get; init; }
+
+        [JsonPropertyName("alg")]
+        public string Alg { get; init; }
+
+        [JsonPropertyName("use")]
+        public string Use { get; init; }
+
+        [JsonPropertyName("kid")]
+        public string Kid { get; init; }
+
+        [JsonPropertyName("n")]
+        public string N { get; init; }
+
+        [JsonPropertyName("e")]
+        public string E { get; init; }
+
+        [JsonPropertyName("x5t")]
+        public string X5t { get; init; }
+
+        [JsonPropertyName("x5c")]
+        public List<string> X5c { get; init; }
+    }
+}
