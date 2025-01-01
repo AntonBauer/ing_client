8e64be176c1fb6d46cdd46284c1b32e6478638b1
Anton Bauer <bauer_av+git@proton.me>
2025-01-01 18:28:40 +0100

    implement sending certificate

--- tst/IngClient.Tests.Client/AuthTests.cs
+++ tst/IngClient.Tests.Client/AuthTests.cs
@@ -1,5 +1,8 @@
 ﻿using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Microsoft.VisualBasic;
 
 namespace IngClient.Tests.Client;
 
@@ -9,42 +12,74 @@ public class AuthTests
     public async Task Auth()
     {
         // Arrange
-        using var client = new HttpClient();
+        var certificate = LoadCertificate();
+        using var client = new HttpClient(new HttpClientHandler()
+        {
+            ClientCertificateOptions = ClientCertificateOption.Manual,
+            SslProtocols = System.Security.Authentication.SslProtocols.Tls12,
+            ClientCertificates = { certificate }
+        });
         client.BaseAddress = new Uri("https://api.sandbox.ing.com/");
 
         var clientId = "e77d776b-90af-4684-bebc-521e5b2614dd";
-        var cert = @"MIIDEDCCAfigAwIBAgIEP8TNVTANBgkqhkiG9w0BAQsFADBKMRMwEQYDVQQKDApT
-YW1wbGUgT3JnMRYwFAYDVQQLDA1JVCBEZXBhcnRtZW50MRswGQYDVQQDDBJleGFt
-cGxlX2NsaWVudF90bHMwHhcNMjQwNTA3MTI1MDIyWhcNMjcwNTA4MTI1MDIyWjBK
-MRMwEQYDVQQKDApTYW1wbGUgT3JnMRYwFAYDVQQLDA1JVCBEZXBhcnRtZW50MRsw
-GQYDVQQDDBJleGFtcGxlX2NsaWVudF90bHMwggEiMA0GCSqGSIb3DQEBAQUAA4IB
-DwAwggEKAoIBAQCpFifUxN3Q6Qsd90cASHZaJ1IBeugUsayk6Bn7fnXvUEm/VrgM
-KK3N577WtW+FBLhjhK2gfMgIurctyfsMhg9swBdG0/ORz5Fr803u9FuMK29laX9w
-IBPZRVPu+Y1nD0SBNDk2HrP7GFld390lyTr4rgH0dVHDBZVEDpKDzOBfBEP4Uv5u
-6+zqM3CnUFoD3E01z1+80PUfakkUpCKG8eKvgpz5FABwYryrChMa0KIsdxf6+P8G
-oqhaeIS8nT/CRxN5JcCpy7svuZN8lGeLC49xDRtCoaC8/Nk4UiOTWOWqeHsnfrnb
-oC5ID/G/ZX6cCqWdVH8+Xg/tRmDblOgtx+wdAgMBAAEwDQYJKoZIhvcNAQELBQAD
-ggEBADzd2pB6aiFSabPCsYwznPQFk18eJtHAJMiySrFF7uK/WkL8PojnRGZQjv32
-9J1tPxh5IFVYWEAZXMyvdYctGbPh3M6RRJwhu7ioyK2NmevwqiD+ATnOioPPwoC2
-EqmnWBCpAPLGQ/tCY7xQeltIo+3kwC6iXjqCnxOW2sd0em1zYjDk1xWg4G6zCNBc
-Jc/SLNZIc4brk4TyaR7+5ysrNIy8KaS+QodEywmFDk4QLh6rPRyMqDengrOUyRce
-sgrAv5bKzBOLRiVO3pXm7kdIW2eP4CuMmOiwU5uxhZXWxpiiHrTdrvSuL6oA5Y4l
-0tk0bnC1xrioMGQgTjFyTzz61bw=".Replace("\r\n", string.Empty);
+        var request = CreateRequest(certificate, clientId);
 
+        // Act
+        var response = await client.SendAsync(request);
+        var raw = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    private static HttpRequestMessage CreateRequest(X509Certificate2 certificate, string clientId)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, "oauth2/token");
+
+        AddContent(request, clientId);
+        AddHeaders(request, certificate, clientId);
+
+        return request;
+    }
+
+    private static void AddContent(HttpRequestMessage request, string clientId)
+    {
         var parameters = new Dictionary<string, string>
         {
             { "grant_type", "client_credentials" },
             { "client_id", clientId }
         };
-        var content = new FormUrlEncodedContent(parameters);
 
-        content.Headers.Add("TPP-Signature-Certificate", cert);
+        request.Content = new FormUrlEncodedContent(parameters);
+    }
 
-        // Act
-        var response = await client.PostAsync("oauth2/token", content);
-        var raw = await response.Content.ReadAsStringAsync();
+    private static void AddHeaders(HttpRequestMessage request, X509Certificate2 certificate, string clientId)
+    {
+        var digest = "";
 
-        // Assert
-        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        var date = DateTime.UtcNow.ToString("r");
+        var toSign = $"POST {request.RequestUri}\ndate:{date}\ndigest: {digest}";
+        var signature = Sign(certificate, toSign);
+
+        // request.Headers.Add("TPP-Signature-Certificate", "");
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("Date", date);
+        request.Headers.Add("Digest", digest);
+        request.Headers.Add("Authorization", $"Signature keyId=\"{clientId}\",algorithm=\"rsa-sha256\",headers=\"(request-target) date digest\",signature=\"{signature}\"");
+    }
+
+    private static X509Certificate2 LoadCertificate()
+    {
+        var cert = X509CertificateLoader.LoadPkcs12FromFile("/home/anton/Downloads/cert.pfx", "a");
+        return cert;
+    }
+
+    private static string Sign(X509Certificate2 certificate, string toSign)
+    {
+        var data = Encoding.UTF8.GetBytes(toSign);
+        var signed = certificate.GetRSAPrivateKey()!.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        var base64 = Convert.ToBase64String(signed);
+
+        return base64;
     }
 }
